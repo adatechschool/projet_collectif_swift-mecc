0eb2fc43bab4b6608f038a0ae49d508259808080
EstherVlp <35601411+EstherVlp@users.noreply.github.com>
2022-05-02 10:23:45 +0200

    first commit

---
+++ Floater/Floater/FloaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloaterApp.swift
+//  Floater
+//
+//  Created by Esther Vilpique on 02/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct FloaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Floater/FloaterUITests/FloaterUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FloaterUITestsLaunchTests.swift
+//  FloaterUITests
+//
+//  Created by Esther Vilpique on 02/05/2022.
+//
+
+import XCTest
+
+class FloaterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
