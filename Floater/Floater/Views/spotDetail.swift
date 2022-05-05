e6bd9742b8011c9b3597b46ae538e37160c9b8fa
EstherVlp <35601411+EstherVlp@users.noreply.github.com>
2022-05-05 12:55:31 +0200

    feat: add file: circleimage.swift & spotdetail.swift

---
+++ Floater/Floater/Views/spotDetail.swift
@@ -0,0 +1,48 @@
+//
+//  spotDetail.swift
+//  Floater
+//
+//  Created by Esther Vilpique on 05/05/2022.
+//
+
+import SwiftUI
+import CoreLocation
+import MapKit
+
+struct spotDetail: View {
+    var landmark: Landmark
+    var body: some View {
+            ScrollView {
+                MapView(coordinate: landmark.locationCoordinate)
+                    .ignoresSafeArea(edges: .top)
+                    .frame(height: 300)
+                CircleImage(image: landmark.image)
+                    .offset(y: -130)
+                    .padding(.bottom, -130)
+                VStack(alignment: .leading) {
+
+                    Text(landmark.spot)
+                        .font(.title)
+                  /*  HStack {
+                        Text(landmark.park)
+                            .font(.subheadline)
+                    }*/
+                    Divider()
+
+                    Text("About \(landmark.spot)")
+                        .font(.title2)
+                    Text(landmark.description)
+                }
+                .padding()
+
+            }
+            .navigationTitle(landmark.spot)
+            .navigationBarTitleDisplayMode(.inline)
+        }
+}
+
+struct spotDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        spotDetail(landmark: landmarks[0])
+    }
+}
---
+++ test.swift
@@ -0,0 +1,20 @@
+//
+//  spotDetail.swift
+//  Floater
+//
+//  Created by Esther Vilpique on 05/05/2022.
+//
+
+import SwiftUI
+
+struct spotDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct spotDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        spotDetail()
+    }
+}
